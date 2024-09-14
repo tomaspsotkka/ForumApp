7d2457adc5ef4b00fd9eb61ed281d1e8b8eede21
tomaspsotkka <psotka439@gmail.com>
2024-09-14 17:04:07 +0200

    files rename

--- Server/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Server/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c084af9c624ffcfd9b8fc1c5200b58f623977ade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8db2f610bcdeacccaa7127d0c1c635c2a0fe02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
